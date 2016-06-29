825efe8b47c671efa836b129acfa6c257858c6d9
Antoni Dobrenov <onesum977@gmail.com>
2016-06-29 12:37:26 +0300

    user check (done mby)

--- app/src/main/java/com/example/user1/volleyballmanager20/MainActivity.java
+++ app/src/main/java/com/example/user1/volleyballmanager20/MainActivity.java
@@ -27,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     Button btnReg;
     EditText edtPass;
     Button btnSearch;
+    Button button;
     public static ArrayList<Player> players;
 
     android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
@@ -60,15 +61,16 @@ public class MainActivity extends AppCompatActivity {
         edtPass = (EditText) findViewById(R.id.edtxt_pass);
         btnReg = (Button) findViewById(R.id.btn_reg);
         btnSearch = (Button) findViewById(R.id.btn_search);
-
+        button = (Button) findViewById(R.id.button);
 
         btnReg.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-
-
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,RegistrationActivity.class);
+                startActivity(intent);
             }
         });
+
 
         btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/user1/volleyballmanager20/cmn/Config.java
+++ app/src/main/java/com/example/user1/volleyballmanager20/cmn/Config.java
@@ -6,6 +6,7 @@ package com.example.user1.volleyballmanager20.cmn;
 public class Config {
     public static final String FIREBASE_URL = "https://volleyballmanager-7832a.firebaseio.com/";
     public static final String FIREBASE_PLAYERS_URL = "https://volleyballmanager-7832a.firebaseio.com/players";
+    public static final String FIREBASE_USERS_URL= "https://volleyballmanager-7832a.firebaseio.com/User";
     public static final String SETTER = "S";
     public static final String OPPOSITE = "O";
     public static final String OUTSIDE_HITTER = "OH";
